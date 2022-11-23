b9c1ec5c81133aad4f82e587abbbcc7576812773
MyoThuKha <myothukha@outlook.sg>
2022-11-23 19:36:19 +0630

    ...

--- App.tsx
+++ App.tsx
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
+import { StatusBar } from "react-native";
+import { StatusBar as ExpoBar } from "expo-status-bar";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ExpoBar style="auto" />
+      <Text style={styles.text}>Today</Text>
+      <Text style={styles.text}>Friday, Oct 8</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    paddingTop: StatusBar.currentHeight,
+    paddingHorizontal: 25,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+  },
+  text: {
+    fontSize: 20,
   },
 });
